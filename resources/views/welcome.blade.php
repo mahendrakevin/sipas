944fecfafbd678025e02107cd6b66c975c38319e
Fathil Arham <fathil.arham@gmail.com>
2021-07-11 19:09:48 +0700

    Created Mail Master Action

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
+@section('content')
+    <button type="button" class="btn btn-outline-primary">{{ $user->email }} ({{ $users_count }})</button>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-</head>
-
-<body class="antialiased">
-    <button type="button" class="btn btn-outline-primary">Primary</button>
-    <button type="button" class="btn btn-close-white"><a href="{{route('login')}}">Login</a></button>
-</body>
-<script src="{{ asset('js/app.js') }}"></script>
-
-</html>
+@endsection

44eaf621ef8c652cc9b864377b6089f1416467d6
Kevin Indra Mahendra <indramahendrakevin98@gmail.com>
2021-07-11 14:41:13 +0700

    Just Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-</head>
-
-<body class="antialiased">
+@extends('layouts.app')
+@section('content')
     <button type="button" class="btn btn-outline-primary">Primary</button>
-    <button type="button" class="btn btn-close-white"><a href="{{route('login')}}">Login</a></button>
-</body>
-<script src="{{ asset('js/app.js') }}"></script>
-
-</html>
+@endsection

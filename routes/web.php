9cfdc4955e2bd6ad48e89a54cd7e43066baeb659
Fathil Arham <fathil.arham@gmail.com>
2021-07-11 19:09:48 +0700

    Created Mail Master Action

--- routes/web.php
+++ routes/web.php
@@ -216,6 +216,3 @@ require __DIR__ . '/auth.php';
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function (){
-    return view('dashboard.index');
-})->name('dashboard');
